40168e4502d4e16e9fc1925cd96f339ea72b892e
Serhiy Hulyi <serhiyhulyi93@gmail.com>
2017-05-17 10:07:44 +0300

    start

---
+++ js/main.js
@@ -0,0 +1,136 @@
+// Global parameters
+window.params = {
+    isMobile: /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent),
+    isIOS: /iPhone|iPad|iPod/i.test(navigator.userAgent)
+};
+
+
+/**
+     *
+     * Check if element exist on page
+     *
+     * @param el {string} jQuery object (#popup)
+     *
+     * @return {bool}
+     *
+*/
+function exist(el){
+    if ( $(el).length > 0 ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+jQuery(document).ready(function($) {
+
+    /*---------------------------
+                                  ADD CLASS ON SCROLL
+    ---------------------------*/
+    $(function() { 
+        var $document = $(document),
+            $element = $('.menu-button'),
+            $element2 = $('header'),
+            className = 'hasScrolled';
+
+        $document.scroll(function() {
+            $element.toggleClass(className, $document.scrollTop() >= 1);
+            $element2.toggleClass(className, $document.scrollTop() >= 1);
+        });
+    });
+    
+    /*---------------------------
+                                PAGE ANCHORS
+    ---------------------------*/
+    $('.mainNav a, .anchor').click(function() {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 50
+        }, 800);
+        return false;
+    });
+
+    /*---------------------------
+                                  MENU TOGGLE
+    ---------------------------*/
+    $('.menu-button').on('click', function(event) {
+        event.preventDefault();
+        $(this).toggleClass('open');
+        $(this).siblings('header').toggleClass('open');
+        if ($('header').hasClass('open')) {
+                $('body').css('overflow', 'hidden');
+            } else {
+                $('body').css('overflow', 'visible');
+            }
+    });
+
+
+
+    $('.form').on('submit', function(event) {
+        event.preventDefault();
+        var data = new FormData(this);
+        $(this).find('button').prop('disabled', true);
+        $.ajax({
+            url: theme.url + '/forms.php',
+            type: 'POST',
+            data: data,
+            cache: false,
+            contentType: false,
+            processData: false,
+            success: function(result) {
+                if (result.status == 'ok') {
+                    openPopup('#modal-popup-ok')
+                } else {
+                    openPopup('#modal-popup-error')
+                }
+            },
+            error: function(result) {
+                openPopup('#modal-popup-error');
+            }
+        }).always(function() {
+            $('form').each(function(index, el) {
+                $(this)[0].reset();
+                $(this).find('button').prop('disabled', false);
+            });
+        });
+    });
+
+
+
+    /*Google map init*/
+    var map;
+    function googleMap_initialize() {
+        var lat = $('#map_canvas').data('lat');
+        var long = $('#map_canvas').data('lng');
+
+        var mapCenterCoord = new google.maps.LatLng(lat, long);
+        var mapMarkerCoord = new google.maps.LatLng(lat, long);
+
+        var mapOptions = {
+            center: mapCenterCoord,
+            zoom: 17,
+            //draggable: false,
+            disableDefaultUI: true,
+            scrollwheel: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+        var markerImage = new google.maps.MarkerImage('images/location.png');
+        var marker = new google.maps.Marker({
+            icon: markerImage,
+            position: mapMarkerCoord, 
+            map: map,
+            title:"Чисто Строй"
+        });
+        
+        $(window).resize(function (){
+            map.setCenter(mapCenterCoord);
+        });
+    }
+
+    if ( exist( '#map_canvas' ) ) {
+        googleMap_initialize();
+    }
+
+}); // end file
